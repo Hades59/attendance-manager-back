523adc76ece3edeaad28c88c910c9afdcf26400c
Pauline JACOB <pauline.lille@gmail.com>
2017-12-04 15:03:46 +0100

    modif pour valid

--- src/main/java/dev/attendancemanager/controller/AbsenceController.java
+++ src/main/java/dev/attendancemanager/controller/AbsenceController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import dev.attendancemanager.entite.Absence;
 import dev.attendancemanager.entite.AbsenceStatus;
 import dev.attendancemanager.repository.AbsenceRepository;
+import dev.attendancemanager.util.Avis;
 /**
  * 
  * @author ETY23
@@ -31,14 +33,10 @@ public class AbsenceController {
 	AbsenceRepository absenceRepository;
 
 	@GetMapping
-	public List<Absence> getAbsences(@RequestParam Optional<AbsenceStatus> status) {
-		if (status.isPresent()) {
-			absenceRepository.findByStatus(status.get());
+	public List<Absence> getAbsences(@RequestParam(value="status", required=false) AbsenceStatus status) {
+		if (status != null) {
+			return absenceRepository.findByStatus(status);
 		}
 		return absenceRepository.findAll();
 	}
-	
-	
-
-
 }
